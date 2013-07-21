d2c56b1c6655d344c4641c3cfdf139d94c64929a
Elena Posea <elena.posea@gmail.com>
2013-07-21 15:12:45 +0300

    my first comit for today

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+end
+class StaticPagesController < ApplicationController
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp1::Application.config.secret_token = 'fd73514db8c327586c85781ea9fb878822a5af0b6e1c2a4bc0acdf3dc473438aef57a2cc7368925c65a3cdebfb0c6daa45f647221fdfb303bd8acbbf1decc738'
